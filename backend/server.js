9c533fcb7509fdbe4d586a85e03b0c082bfb3e57
keshavkumar11 <keshavkumarpurani@gmail.com>
2025-12-31 15:01:03 +0530

    add backend folder structure and environment configuration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ app.get("/",(req,res)=>{
     res.send("yugdrashta backend running")
 })
 
-const PORT  = 5000;
+const PORT  = process.env.PORT || 5000;
+
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
     
