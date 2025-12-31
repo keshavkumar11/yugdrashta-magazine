d79e10cba76a1d0b4bb95de895b9f2c21bb7bca6
keshavkumar11 <keshavkumarpurani@gmail.com>
2025-12-31 17:09:38 +0530

    create user model with detailed address and contact information

---
+++ backend/models/User.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type:String,
+            required:true,
+            trim:true
+        },
+        email: {
+            type:String,
+            required:true,
+            trim:true,
+            unique:true,
+            lowercase:true
+        },
+
+        phone:{
+            type:String,
+            required:true
+        },
+
+        whatsappNumber:{
+            type:String,
+            required:true
+        },
+
+        bloodGroup:{
+            type:String
+        },
+
+        password:{
+            type:String,
+            required:true
+        },
+
+        role:{
+            type:String,
+            enum : ["user","admin"],
+            default:"user"
+        },
+
+        address: {
+            country:String,
+            state:String,
+            district:String,
+            taluka:String,
+            villageOrCity:String,
+
+            houseNo: String,
+            societyOrStreet:String,
+            areaorLandmark:String,
+            pincode:String
+        },
+
+        subscription:{
+            active:{
+                type:Boolean,
+                default:false
+            },
+            planType: String,
+            startDate:Date,
+            endDate: Date
+        }
+    },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model("User",userSchema);
